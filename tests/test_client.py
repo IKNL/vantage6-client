d6109bc4e54599dcc1d738a8b4b8c9b51625beb2
Djura Smits <djura.smits@gmail.com>
2020-08-18 15:27:48 +0200

    Converted tests to unittest.TestCase so it will be run in travis

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,3 +1,5 @@
+from unittest import TestCase
+
 from vantage6.client import Client
 from unittest.mock import patch, MagicMock
 import base64
@@ -20,46 +22,46 @@ ORGANIZATION_IDS = [1]
 SAMPLE_INPUT = {'method': 'test-task'}
 
 
-def test_post_task_legacy_method():
-    post_input = post_task_on_mock_client(SAMPLE_INPUT, 'legacy')
-    decoded_input = base64.b64decode(post_input)
-    decoded_input = pickle.loads(decoded_input)
-    assert {'method': 'test-task'} == decoded_input
+class TestClient(TestCase):
 
+    def test_post_task_legacy_method(self):
+        post_input = TestClient.post_task_on_mock_client(SAMPLE_INPUT, 'legacy')
+        decoded_input = base64.b64decode(post_input)
+        decoded_input = pickle.loads(decoded_input)
+        assert {'method': 'test-task'} == decoded_input
 
-def test_post_json_task():
-    post_input = post_task_on_mock_client(SAMPLE_INPUT, 'json')
-    decoded_input = base64.b64decode(post_input)
-    assert b'json.{"method": "test-task"}' == decoded_input
+    def test_post_json_task(self):
+        post_input = TestClient.post_task_on_mock_client(SAMPLE_INPUT, 'json')
+        decoded_input = base64.b64decode(post_input)
+        assert b'json.{"method": "test-task"}' == decoded_input
 
+    def test_post_pickle_task(self):
+        post_input = TestClient.post_task_on_mock_client(SAMPLE_INPUT, 'pickle')
+        decoded_input = base64.b64decode(post_input)
 
-def test_post_pickle_task():
-    post_input = post_task_on_mock_client(SAMPLE_INPUT, 'pickle')
-    decoded_input = base64.b64decode(post_input)
+        assert b'pickle.' == decoded_input[0:7]
 
-    assert b'pickle.' == decoded_input[0:7]
+        assert {'method': 'test-task'} == pickle.loads(decoded_input[7:])
 
-    assert {'method': 'test-task'} == pickle.loads(decoded_input[7:])
+    @staticmethod
+    def post_task_on_mock_client(input_, serialization: str):
+        mock_jwt = MagicMock()
+        mock_jwt.decode.return_value = {'identity': FAKE_ID}
+        mock_requests = MagicMock()
+        mock_requests.get.return_value.status_code = 200
+        mock_requests.post.return_value.status_code = 200
+        with patch.multiple('vantage6.client', requests=mock_requests, jwt=mock_jwt):
+            client = Client(HOST, PORT)
+            client.authenticate(USERNAME, PASSWORD)
+            client.setup_encryption(None)
 
+            client.post_task(name=TASK_NAME, image=TASK_IMAGE, collaboration_id=COLLABORATION_ID,
+                             organization_ids=ORGANIZATION_IDS, input_=input_, data_format=serialization)
 
-def post_task_on_mock_client(input_, serialization: str):
-    mock_jwt = MagicMock()
-    mock_jwt.decode.return_value = {'identity': FAKE_ID}
-    mock_requests = MagicMock()
-    mock_requests.get.return_value.status_code = 200
-    mock_requests.post.return_value.status_code = 200
-    with patch.multiple('vantage6.client', requests=mock_requests, jwt=mock_jwt):
-        client = Client(HOST, PORT)
-        client.authenticate(USERNAME, PASSWORD)
-        client.setup_encryption(None)
+            # In a request.post call, json is provided with the keyword argument 'json'
+            # call_args provides a tuple with positional arguments followed by a dict with positional arguments
+            post_content = mock_requests.post.call_args[1]['json']
 
-        client.post_task(name=TASK_NAME, image=TASK_IMAGE, collaboration_id=COLLABORATION_ID,
-                         organization_ids=ORGANIZATION_IDS, input_=input_, data_format=serialization)
+            post_input = post_content['organizations'][0]['input']
 
-        # In a request.post call, json is provided with the keyword argument 'json'
-        # call_args provides a tuple with positional arguments followed by a dict with positional arguments
-        post_content = mock_requests.post.call_args[1]['json']
-
-        post_input = post_content['organizations'][0]['input']
-
-    return post_input
+        return post_input
